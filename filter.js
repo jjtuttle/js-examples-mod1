c27abe1af31d05531b7793b5a8e1bbc5c84dc0c7
James Tuttle <jaymz69@yahoo.com>
2021-10-09 06:22:46 -0700

    init file load for filter examples.

---
+++ filter.js
@@ -0,0 +1,20 @@
+//* Array of Objects of cities and their populations
+let cities = [
+    { name: 'Los Angeles', population: 3792621 },
+    { name: 'New York', population: 8175133 },
+    { name: 'Chicago', population: 2695598 },
+    { name: 'Houston', population: 2099451 },
+    { name: 'Philadelphia', population: 1526006 }
+];
+
+//* for() way to find population > 3 million
+let largePopulation = (cities) => {
+    let bigCities = [];
+
+    for(let i = 0; i < cities.length; i++) {
+        if(cities[i].population > 3_000_000) {
+            bigCities.push(cities[i]);
+        }
+    }
+};
+console.log(largePopulation(cities));
