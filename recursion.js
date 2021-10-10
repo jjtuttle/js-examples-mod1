1f261794ade4cdc10baa36bd02e3e34959eea066
James Tuttle <jaymz69@yahoo.com>
2021-10-10 09:27:35 -0700

    add sum recusrion example.

--- recursion.js
+++ recursion.js
@@ -8,3 +8,13 @@ const factorial = function (n) {
 // console.log(factorial(5));
 
 //! ===============================================
+//! Sum() with Array
+const sum = function (array) {
+    if (!array.length) return 0;
+    //if()
+
+    return array[0] + sum(array.slice(1));
+};
+console.log(sum([1, 2, 3, 4, 5, 6]));
+
+//! ===============================================
