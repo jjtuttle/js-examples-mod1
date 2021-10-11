8899d501f07106177e04b8a79a8830c109c5a833
James Tuttle <jaymz69@yahoo.com>
2021-10-11 06:23:20 -0700

    another filter() example.

--- filter.js
+++ filter.js
@@ -45,4 +45,25 @@ let chainedFilter = cities => {
 
         return fC;
 };
-console.log(chainedFilter(cities));
+//? console.log(chainedFilter(cities));
+
+//! ===============================================
+//! filter from - https://www.youtube.com/watch?v=R8rmfD9Y5-c&t=1s
+
+const items = [
+    {name: 'Bike',          price: 100},
+    { name: 'TV',           price: 200 },
+    { name: 'Album',        price: 10 },
+    { name: 'Book',         price: 5 },
+    { name: 'Phone',        price: 500 },
+    { name: 'Computer',     price: 1000 },
+    { name: 'Keyboard',     price: 25 },
+    
+];
+
+const filteredItems = items.filter((item) => {
+    return item.price <= 100;
+});
+console.log(filteredItems);
+
+//! ===============================================
