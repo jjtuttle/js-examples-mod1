89e7cd933005c4cdf643866c13dbb999dc138fd0
James Tuttle <jaymz69@yahoo.com>
2021-10-19 05:33:19 -0700

    added another example.

--- isInt.js
+++ isInt.js
@@ -1,43 +1,56 @@
 
 function isInt(n) {
-    if(n % 1 === 0) return true;
+    if (n % 1 === 0) return true;
 
-    if(n % 1!== 0) return false;
+    if (n % 1 !== 0) return false;
 }
 
 function countToZero(n) {
-    // positive non integer
-    if (n > 0 && !isInt(n)) {
-        console.log(n);
+    console.log(n);
 
-        let nextN = n - n % 1;
-        countToZero(nextN);
-    }
-    // if negative non integer
-    if (n < 0 && !isInt(n)) {
-        console.log(n);
+    // base case
+    if (n === 0) return;
 
-        let nextN = n + -(n % 1);
-        countToZero(nextN);
-    }
-    // if positive integer
-    if (n > 0 && isInt(n)){
-        console.log(n);
+    let decrement = n % 1;
 
-        let nextN = n - 1;
-        countToZero(nextN);
+    if (decrement === 0) {
+        decrement = n / Math.abs(n);
     }
-    // if negative integer 
-    if (n < 0 && isInt(n)) {
-        console.log(n);
+    // recursive step
+    countToZero(n - decrement);
 
-        let nextN = n + 1
-        countToZero(nextN);
-    }
-    // if zero
-    if(n === 0) {
-        console.log(0);
-    }
+    // // positive non integer
+    // if (n > 0 && !isInt(n)) {
+    //     console.log(n);
+
+    //     let nextN = n - n % 1;
+    //     countToZero(nextN);
+    // }
+    // // if negative non integer
+    // if (n < 0 && !isInt(n)) {
+    //     console.log(n);
+
+    //     let nextN = n + -(n % 1);
+    //     countToZero(nextN);
+    // }
+    // // if positive integer
+    // if (n > 0 && isInt(n)){
+    //     console.log(n);
+
+    //     let nextN = n - 1;
+    //     countToZero(nextN);
+    // }
+    // // if negative integer 
+    // if (n < 0 && isInt(n)) {
+    //     console.log(n);
+
+    //     let nextN = n + 1
+    //     countToZero(nextN);
+    // }
+    // // if zero
+    // if(n === 0) {
+    //     console.log(0);
+    // }
 }
 
 console.log(countToZero(3.5));
