ba6703766f3bfedea60fd4341a6585563813081b
James Tuttle <jaymz69@yahoo.com>
2021-10-21 05:48:52 -0700

    adding hash-table.js examples.

--- hash-tables.js
+++ hash-tables.js
@@ -29,7 +29,22 @@ function hashMod(key) {
     return hash(key) % data.length; // data.length is the Array declared above
 }
 // - 
-console.log(hash("key"));            // 329
-console.log(hashMod("key"));         // 1
-console.log(hash('james'));          // 528
-console.log(hashMod('james'));       // 0
+// console.log(hash("key"));            // 329
+// console.log(hashMod("key"));         // 1
+// console.log(hash('james'));          // 528
+// console.log(hashMod('james'));       // 0
+//! ===============================================
+cache = {};
+
+function fibMemoPrint(n) {
+    console.log(`Running fibMemoPrint(${n})`);
+    console.log(cache);
+    if (n === 1) return 0;
+    if (n === 2) return 1;
+
+    if (cache[n] === undefined) {
+        cache[n] = fibMemoPrint(n - 1) + fibMemoPrint(n - 2);
+    }
+
+    return cache[n];
+}
