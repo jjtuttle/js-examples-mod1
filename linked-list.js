683d90a8b6738c5c0fdb22927ac14884310483bd
James Tuttle <jaymz69@yahoo.com>
2021-10-25 07:19:54 -0700

    adding ll example.

--- linked-list.js
+++ linked-list.js
@@ -32,17 +32,48 @@ console.log(`Dequeue time: ${dequeueEndTime - dequeueStartTime}ms`);
 //     return prev;
 // }
 
+//! ===============================================
+//! node
+class Node {
+    constructor(val) {
+        this.next = null;
+        this.value = val;
+    }
+}
 
+class Queue {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
 
+    enqueue(val) {
+        let newNode = new SinglyLinkedNode(val);
+        if (!this.length) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        this.length++;
+        return this.length;
+    }
 
-
-
-
-
-
-
-
-
+    dequeue() {
+        let removed = this.head;
+        if (!this.length) return null;
+        if (this.length === 1) {
+            this.head = null;
+            this.tail = null;
+        } else {
+            this.head = removed.next;
+        }
+        this.length--;
+        return removed.value;
+    }
+}
 
 
 
